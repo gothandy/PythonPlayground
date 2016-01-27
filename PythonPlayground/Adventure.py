d9a72dc0197536aeb32802ae2cec9783ea4efc64
gothandy <gothandy@hotmail.com>
2016-01-27 14:07:49 +0000

    json style story structure

--- PythonPlayground/Adventure.py
+++ PythonPlayground/Adventure.py
@@ -1,31 +1,37 @@
-wellBottom = {
-    'look': {'print': 'You can see a bucket above' }
+story = {
+    'wellBottom': {
+        'look': {'print': 'You can see a bucket above' },
+        'back': {'print': 'You go back through the door',
+                 'answers': 'lightOn'}
+        },
+    'lightOn': {
+        'door': {'print': 'You open the door and are in the bottom of a well',
+                    'answers': 'wellBottom'}
+        },
+    'start': {
+        'light': {'print': 'You can see a door',
+                  'answers': 'lightOn' }, 
+        'eat': {'print': 'You have nothing to eat'}
+        }
     }
 
-lightOn = {
-    'door': {'print': 'You open the door and are in the bottom of a well', 'answers': wellBottom}
-    }
-
-start = {
-    'light': {'print': 'You can see a door', 'answers': lightOn }, 
-    'eat': {'print': 'You have nothing to eat'}
-    }
-
-answers = start
-
+answers = story['start']
+  
 while True:
 
     answer = input(': ').lower()
 
     if answer == 'exit':
         break
+
     elif answer == 'help':
-        for key in iter(answers):
-            print(key)
+        print('Try', ', '.join(iter(answers)))
+
     elif answer not in answers:
-        print("You can't", answer)
+        print('You can\'t', answer)
+
     else:
         print(answers[answer]['print']) 
 
         if 'answers' in answers[answer]:
-            answers = answers[answer]['answers']
+            answers = story[answers[answer]['answers']]
