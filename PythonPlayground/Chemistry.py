ea678146cd6faefe5aff115d4c5d04013fc062ab
gothandy <gothandy@hotmail.com>
2016-01-29 12:44:39 +0000

    Added some minecraft style making.

--- PythonPlayground/Chemistry.py
+++ PythonPlayground/Chemistry.py
@@ -1,32 +1,58 @@
 recipes = {
-    'plank': ['wood', 'knife'],
-    'knife': ['iron', 'fire'],
+    'plank': ['wood', 'blade'],
+    'blade': ['iron', 'fire'],
     'fire': ['wood', 'flint'],
     }
 
 materials = {
-    'wood': 0,
+    'wood': 2,
     'blade': 0,
     'plank': 0,
     'fire': 0,
-    'iron': 0,
-    'flint': 0
+    'iron': 1,
+    'flint': 1
     }
 
-
-action = input(": ").split()
-
-for product in iter(recipes):
-
-    count = 0
+def checkIngredients(product):
 
     for ingredient in recipes[product]:
+        if materials[ingredient] < 1:
+            print("You need {0}".format(ingredient))
+            return False
 
-        if ingredient in action:
-            count += 1
+    return True
 
-    if count == len(action):
-        print(product)
+def makeProduct(product):
 
-        
+    for ingredient in recipes[product]:
+        materials[ingredient] += -1
+
+    materials[product] += 1
+
+    return True
+
+def cook(action):
+
+    for product in iter(recipes):
+
+        count = 0
+
+        for ingredient in recipes[product]:
+
+            if ingredient in action:
+                count += 1
+
+        if count == len(action):
+
+            if checkIngredients(product):
+
+                makeProduct(product)
+
+                print(product)
+
+    return True
+
+while cook(input(": ").split()):
+    pass
+
 
