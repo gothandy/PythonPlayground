473222e44f63b2d88e6ee1be6e66fb60de79fbeb
gothandy <gothandy@hotmail.com>
2016-02-06 16:23:13 +0000

    Playing with functions.

--- PythonPlayground/Functions.py
+++ PythonPlayground/Functions.py
@@ -4,6 +4,32 @@ def A():
 def B():
     print('B')
 
-f = { 'a': 'A()', 'b': 'B()' }
+def C():
+    print('C')
 
-eval(f['b'])
+i = 'b'
+
+# Using Eval
+f = { 'a': 'A()', 'b': 'B()' }
+eval(f[i])
+
+# Classic if
+if i == 'a':
+    A()
+elif i == 'b':
+    B()
+
+# Short circuit.
+def sc(x):
+    (x == 'a' and A()) or (x == 'b' and B())
+sc(i)
+
+# Lambda
+l = lambda x: (x == 'a' and A()) or (x == 'b' and B()) or (x == 'c' and C())
+l(i)
+
+# Map
+list(map(l, ['a','b','c']))
+
+# Comprehensions
+[l(x) for x in ['a','b','c']]
