a8dfb2093302f2063849f2c0266e8a3eab493974
gothandy <gothandy@hotmail.com>
2016-02-06 09:18:31 +0000

    tidying.

---
+++ PythonPlayground/calculator.py
@@ -0,0 +1,15 @@
+
+a = float(input("number: "))
+
+while True:
+    b = input("action: ")
+    c = float(input("number: "))
+
+    if b == '+':
+        a = a + c
+
+    elif b == '-':
+        a = a - c
+
+    print(a)
+        
