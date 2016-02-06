e3d55c4b012b9cf1389fdd6c906ca3cdcb99f396
gothandy <gothandy@hotmail.com>
2016-02-06 08:31:13 +0000

    Changes made on Ubuntu

--- PythonPlayground/Calculator.py
+++ PythonPlayground/Calculator.py
@@ -1,1 +1,18 @@
+def isFloat(obj):
+    try:
+        f = float(obj)
+        return True
+    except ValueError:
+        return False
 
+value = 0
+
+while True:
+
+    cmd = ""
+    cmd = input(':')
+    
+    if isFloat(cmd):
+        value = float(cmd)
+
+    print(value)
